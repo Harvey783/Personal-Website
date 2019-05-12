8b9cbba876f159263e63ac48945753321f8ba4be
Christopher Harvey <chharvey@icloud>
2019-05-11 23:32:02 -0400

    changes

--- index.php
+++ index.php
@@ -149,7 +149,7 @@
                 </li>
               </ul>
               <p class="text-center">
-              Full stack web developer skilled in JavaScript, React, Redux, Ruby on Rails, and multiple CSS frameworks with marketing experience demonstrating an ability to create and design content that drives revenue. I'm confident this bottom-line perspective along with my technical skills can help create websites that are not only attractive and engaging but that also drive revenue and promote growth.
+              Full stack web developer skilled in JavaScript, React, Redux, Ruby on Rails, and multiple HTML/CSS frameworks with experience demonstrating an ability to create and design content that drives revenue. I'm confident this bottom-line perspective along with my technical skills can help create products and technologies that are not only attractive and engaging but that also drive revenue and promote growth.
               </p>
               <div class="row">
                 <div class="col col-xs-12 col-sm-12 col-md-12 text-center">
