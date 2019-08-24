afd4b43739cb430d2b870bd94bab3c6c9f4d2c95
Chris <chharvey@icloud.com>
2019-08-23 20:59:21 -0400

    intro update

--- index.php
+++ index.php
@@ -149,12 +149,12 @@
                 </li>
               </ul>
               <p class="text-center">
-              Full stack web developer skilled in JavaScript, React, Redux, Ruby on Rails, and multiple HTML/CSS frameworks with experience demonstrating an ability to create and design content that drives revenue. I'm confident this bottom-line perspective along with my technical skills can help create products and technologies that are not only attractive and engaging but that also drive revenue and promote growth.
+              Full stack web developer skilled in JavaScript, React, Redux, Ruby on Rails, and multiple HTML/CSS frameworks with marketing experience demonstrating an ability to create and design content that drives revenue. I'm confident this bottom-line perspective along with my technical skills can help create products and technologies that are not only attractive and engaging but that also drive revenue and promote growth.
               </p>
               <div class="row">
                 <div class="col col-xs-12 col-sm-12 col-md-12 text-center">
                   <a
-                    href="https://www.dropbox.com/s/hem0kn3gaje77bz/Christopher.Harvey.Resume.pdf?dl=0"
+                    href="https://www.dropbox.com/s/ulpg8jzlp04qcgc/Christopher.Harvey.Resume.pdf?dl=0"
                     download="Christopher Harvey Resume.pdf"
                     class="btn waves-effect waves-light btn-primary custom-btn"
                     >Resume</a
