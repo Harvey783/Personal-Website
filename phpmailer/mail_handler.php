d3b34d5cd1ac4859534843a7dc18971e0e298154
Chris <chharvey@icloud.com>
2019-09-07 02:43:47 -0400

    phpmailer test

--- phpmailer/mail_handler.php
+++ phpmailer/mail_handler.php
@@ -1,42 +1,41 @@
 <?php 
-// Test
-    require('phpmailer/PHPMailerAutoload.php');
+    // require('phpmailer/PHPMailerAutoload.php');
     
-    $mailto = 'chharvey@gmail.com';
-    $name = strip_tags($_POST['name']);
-    $email = strip_tags($_POST['email']);
-    $message = strip_tags($_POST['message']);
+    // $mailto = 'chharvey@gmail.com';
+    // $name = strip_tags($_POST['name']);
+    // $email = strip_tags($_POST['email']);
+    // $message = strip_tags($_POST['message']);
     
-    if($name != null && $email != null && $message != null){
-        $mail = new PHPMailer;
-        $mail->isSMTP();                                      
-        $mail->Host = 'smtp.gmail.com';  
-        $mail->SMTPAuth = true;                              
-        $mail->Username = 'chharvey@gmail.com';                 
-        $mail->Password = '';                           
-        $mail->SMTPSecure = 'tls';                             
-        $mail->Port = 587;                                    
-        $mail->setFrom('chharvey@gmail.com', 'Chris');
-        $mail->addAddress('chharvey@gmail.com', 'Chris');     
-        $mail->addReplyTo($email, $name);
-        $mail->isHTML(true);
-        $mail->Subject = 'New Contact Form Message';
-        $mail->Body = 'Name: '.$name.' <br />Message: '.$message;
+    // if($name != null && $email != null && $message != null){
+    //     $mail = new PHPMailer;
+    //     $mail->isSMTP();                                      
+    //     $mail->Host = 'smtp.gmail.com';  
+    //     $mail->SMTPAuth = true;                              
+    //     $mail->Username = 'chharvey@gmail.com';                 
+    //     $mail->Password = '';                           
+    //     $mail->SMTPSecure = 'tls';                             
+    //     $mail->Port = 587;                                    
+    //     $mail->setFrom('chharvey@gmail.com', 'Chris');
+    //     $mail->addAddress('chharvey@gmail.com', 'Chris');     
+    //     $mail->addReplyTo($email, $name);
+    //     $mail->isHTML(true);
+    //     $mail->Subject = 'New Contact Form Message';
+    //     $mail->Body = 'Name: '.$name.' <br />Message: '.$message;
         
-        if(!$mail->send()) {
-            $signal = 'bad';
-            $msg = 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-            $signal = 'ok';
-            $msg = 'Form submitted';
-        }
-    }
+    //     if(!$mail->send()) {
+    //         $signal = 'bad';
+    //         $msg = 'Mailer Error: ' . $mail->ErrorInfo;
+    //     } else {
+    //         $signal = 'ok';
+    //         $msg = 'Form submitted';
+    //     }
+    // }
     
-    $data = array(
-        'signal' => $signal,
-        'msg' => $msg
-    );
-    echo json_encode($data);
+    // $data = array(
+    //     'signal' => $signal,
+    //     'msg' => $msg
+    // );
+    // echo json_encode($data);
     ?>
 
 
